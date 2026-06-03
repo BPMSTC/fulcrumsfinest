fbf841a2220b634615d3d6be68145b27778c4670
Jordan Tellekson <jordant022105@gmail.com>
2026-06-03 01:39:52 -0500

    [Jordan-main]: Added AdContest checkbox to Account Flags in MainWindow. Fixed subscription edit fields showing old data by updating InverseBooleanConverter to support Visibility target type

--- SnowmobileWPF/Converters/InverseBooleanConverter.cs
+++ SnowmobileWPF/Converters/InverseBooleanConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace SnowmobileWPF.Converters
@@ -6,7 +7,15 @@ namespace SnowmobileWPF.Converters
     public class InverseBooleanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value is bool b ? !b : value;
+        {
+            if (value is bool b)
+            {
+                if (targetType == typeof(Visibility))
+                    return b ? Visibility.Collapsed : Visibility.Visible;
+                return !b;
+            }
+            return value;
+        }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => value is bool b ? !b : value;
